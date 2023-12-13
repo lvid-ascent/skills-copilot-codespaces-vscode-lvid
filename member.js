d978727e2d1d75ad18762776bd7ad0b85c5a4247
Lazar Videnov <67474102+lvid-ascent@users.noreply.github.com>
2023-12-13 20:20:01 +0000

    Second commit

---
+++ member.js
@@ -0,0 +1,28 @@
+function skillsMember() {
+    var member = document.getElementById("member");
+    var skills = document.getElementById("skills");
+    var projects = document.getElementById("projects");
+    var contact = document.getElementById("contact");
+    var about = document.getElementById("about");
+    var home = document.getElementById("home");
+    var memberContent = document.getElementById("memberContent");
+    var skillsContent = document.getElementById("skillsContent");
+    var projectsContent = document.getElementById("projectsContent");
+    var contactContent = document.getElementById("contactContent");
+    var aboutContent = document.getElementById("aboutContent");
+    var homeContent = document.getElementById("homeContent");
+
+    member.style.color = "#fff";
+    skills.style.color = "#000";
+    projects.style.color = "#000";
+    contact.style.color = "#000";
+    about.style.color = "#000";
+    home.style.color = "#000";
+
+    memberContent.style.display = "block";
+    skillsContent.style.display = "none";
+    projectsContent.style.display = "none";
+    contactContent.style.display = "none";
+    aboutContent.style.display = "none";
+    homeContent.style.display = "none";
+}
